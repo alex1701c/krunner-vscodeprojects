3dd7311daaa6e179def59e58ae540f3ec7e5b999
alex1801c <alex1701c.dev@gmx.net>
2020-08-21 05:16:31 -0400

    Kill krunner when binary package is installed/removed

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ fi
 mkdir -p build
 cd build
 
-cmake -DKDE_INSTALL_QTPLUGINDIR=`kf5-config --qt-plugins` -DCMAKE_BUILD_TYPE=Release  ..
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DKDE_INSTALL_QTPLUGINDIR=`kf5-config --qt-plugins` -DCMAKE_BUILD_TYPE=Release  ..
 make -j$(nproc)
 
 sudo make install
