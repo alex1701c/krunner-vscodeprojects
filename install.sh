236688f78d8042fcdd7b5ed0aaf49a48c01fe7ad
alex1701c <alex1701c.dev@gmx.net>
2020-08-21 10:07:54 +0200

    Simplify logic, restart krunner when uninstalling

--- install.sh
+++ install.sh
@@ -11,12 +11,12 @@ fi
 mkdir -p build
 cd build
 
-cmake -DQT_PLUGIN_INSTALL_DIR=`kf5-config --qt-plugins` -DCMAKE_BUILD_TYPE=Release  ..
+cmake -DKDE_INSTALL_QTPLUGINDIR=`kf5-config --qt-plugins` -DCMAKE_BUILD_TYPE=Release  ..
 make -j$(nproc)
 
 sudo make install
 
-kquitapp5 krunner 2> /dev/null
-kstart5 --windowclass krunner krunner > /dev/null 2>&1 &
+kquitapp5 krunner
+kstart5 --windowclass krunner krunner
 
 echo "Installation finished !";
--- uninstall.sh
+++ uninstall.sh
@@ -5,3 +5,7 @@ set -e
 
 cd build
 sudo make uninstall
+
+# KRunner needs to be restarted for the changes to be applied
+kquitapp5 krunner
+kstart5 --windowclass krunner krunner
