275064f5f517a6efa3ab7e2473000b35a530cf29
alex1701c <alex1701c.dev@gmx.net>
2020-08-22 20:10:53 +0200

    Tweak installation

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # Exit if something fails
 set -e
 
+# If we downloaded only the installer we want to clone the repo
 if [[ $(basename "$PWD") != "krunner-vscodeprojects"* ]];then
     git clone https://github.com/alex1701c/krunner-vscodeprojects
     cd krunner-vscodeprojects/
@@ -16,7 +17,19 @@ make -j$(nproc)
 
 sudo make install
 
-kquitapp5 krunner
-kstart5 --windowclass krunner krunner
+# KRunner needs to be restarted for the changes to be applied
+if pgrep -x krunner > /dev/null
+then
+    kquitapp5 krunner
+fi
+
+# If KRunner does not get started using the shortcut we have to autostart it
+krunner_version=$(krunner --version | cut -d " " -f2)
+major_version=$(echo $krunner_version | cut -d "." -f -1)
+minor_version=$(echo $krunner_version | cut -d "." -f2)
+if [[ (("$major_version" < "5")) || (("$minor_version" < "17")) ]]
+then
+    kstart5 --windowclass krunner
+fi
 
 echo "Installation finished !";
