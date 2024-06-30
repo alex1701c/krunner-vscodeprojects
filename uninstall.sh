b75e88a08b5a56163aeaab2b01290850c176f8a9
alex1701c <alex1701c.dev@gmx.net>
2024-06-30 07:56:06 +0200

    Add missing line for determining krunner_version

--- uninstall.sh
+++ uninstall.sh
@@ -7,6 +7,7 @@ cd build
 sudo make uninstall
 
 # KRunner needs to be restarted for the changes to be applied
+krunner_version=$(krunner --version | grep -oP "(?<=krunner )\d+")
 if pgrep -x krunner > /dev/null
 then
     kquitapp"$krunner_version" krunner
